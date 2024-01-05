496b78f9e2582b78ba32e76d73798d86f88f186f
safal1411 <148532255+safal1411@users.noreply.github.com>
2024-01-05 14:27:01 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,26 @@
+<html>
+
+<body>
+
+<h2>My First JavaScript</h2>
+
+<script>
+
+window.alert("Hello World!");
+
+</script>
+
+<p id="demo"> Hello world alert is displayed</p><br>
+
+<script>
+
+document.getElementById("demo").innerHTML = “Hello World
+Again!!”;
+
+document.write("Hello World Again and Again!!!");
+
+</script>
+
+</body>
+
+</html>
